d9e924d347127e909e2ae2e122f971128d662294
keyhan valieh <keyhan.va@gmail.com>
2021-08-18 12:00:35 +0430

    fix some bug and change logo

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,10 +4,6 @@ import './views/assets/styles/app.scss';
 import Form from "./core/Form";
 import ajax from './core/Ajax';
 
-import DisableAutocomplete from 'vue-disable-autocomplete';
-Vue.use(DisableAutocomplete);
-
-
 Vue.component('Form', require('./core/Form').default);
 Vue.component('Button', require('./views/components/form/Button').default);
 Vue.component('navigation', require('./views/components/navbar/navigation').default);
